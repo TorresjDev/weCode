341b1c5485b9db2924ba8bba538c3737cd27d26b
Jesus Torres <torres.jesus2143@gmail.com>
2023-06-29 11:44:15 -0500

    added to backend

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,33 +1,41 @@
-require("dotenv").config();
+///////
+require("dotenv").config(); // Load environment variables from a .env file
+
 const express = require("express");
 const mongoose = require("mongoose");
-const quoteRoutes = require("../routes/quotesRoute");
-//express app
-const app = express();
-const cors = require("cors");
 
-//local port
+const quoteRoutes = require("../routes/quotesRoute");
+
 const port = process.env.PORT;
+
+// Create an instance of the Express application
+const app = express();
+
+// Enable Cross-Origin Resource Sharing (CORS) for the app
+const cors = require("cors");
 const origin = process.env.REACT_ORIGIN_REQUEST;
 app.use(
 	cors({
 		origin: origin
 	})
 );
-app.use(express.json());
+
+app.use(express.json()); // Parse JSON request bodies
+
+// Log request path, HTTP method, and response
 app.use((req, res, next) => {
 	console.log(req.path, req.method, res);
 	next();
 });
 
-//route handler
+// route handler for the "/api/quotes" path
 app.use("/api/quotes", quoteRoutes);
 
-//connect to db
+// Connect to the MongoDB database
 mongoose
 	.connect(process.env.MONGO_URI)
 	.then(() => {
-		//listen for requests
+		// Start the Express app and listen for incoming requests
 		app.listen(port, () => console.log(`Testing weCode: ${port}!`));
 	})
 	.catch((e) => {
