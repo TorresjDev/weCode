0c2d0e2d7be46303e3ff5bf1adf8ee49e94bad2e
Jesus Torres <torres.jesus2143@gmail.com>
2023-06-30 02:22:20 -0500

    added fronted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import QuoteForm from "./components/quotes/QuoteForm";
 import Quotes from "./pages/Quotes";
 import Login from "./pages/Login";
 import "./app.css";
@@ -18,6 +19,10 @@ function App() {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/quotes" element={<Quotes />} />
+						<Route path="/quote/new" element={<QuoteForm />} />
+
+						<Route path="/quote/:id" element={<QuoteForm />} />
+
 						<Route path="/login" element={<Login />} />
 						<Route path="*" element={<NotFound />} />
 					</Routes>
--- frontend/src/pages/Quotes.jsx
+++ frontend/src/pages/Quotes.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useCallback } from "react";
 import QuoteCard from "../components/quotes/QuoteCard";
-import QuoteForm from "../components/quotes/QuoteForm";
 import quotesService from "../services/quotesService";
 import { useQuotesContext } from "../hooks/useQuotesContext";
+import { Link } from "react-router-dom";
 
 function Quotes() {
 	const { state, dispatch } = useQuotesContext();
@@ -33,8 +33,27 @@ function Quotes() {
 
 	return (
 		<div className="container-fluid">
-			<div className="col-md-7 mx-auto mt-3">
-				<QuoteForm updateQState={onGetQuotesSuccess} />
+			<div className="row m-3 py-1 rounded shadow">
+				<div className="col-md-1 mx-1 pt-1">
+					<Link to="/quote/new" className="btn py-1 btn-outline-success border-3 shadow">
+						<div title="add a quote" className="pt-2 material-symbols-outlined">
+							Add_Comment
+						</div>
+					</Link>
+				</div>
+				<div className="col-md-1 pt-2">
+					<h3>Quotes</h3>
+				</div>
+				<div className="col-md-4 mx-auto mt-1">
+					<div class="input-group pt-auto shadow border border-2 rounded border-dark ">
+						<input type="text" class="form-control" placeholder="search..." aria-label="" aria-describedby="basic-addon1" />
+						<div class="input-group-prepend">
+							<button class="btn btn-info border border-dark" type="button">
+								Button
+							</button>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div className="row gx-1 my-3 bg-light border border-3 border-dark rounded">
 				{state.quotes && state.quotes.map(mapQuotes)}
